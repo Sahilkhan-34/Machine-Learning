465e400ab034e544b08ff54549f9103a259d7a00
sahil khan <sahilkhan.edu@gmail.com>
2024-03-29 07:46:48 +0530

    Flask

---
+++ Flask/Example1.py
@@ -0,0 +1,12 @@
+"""
+that API endpoint will hit any webbrowser to get the result
+If you see any application amazon/gmail all are http url
+These http urls create  by web framworks
+POSTMAN is an application to test these urls working or not
+
+"""
+
+def greet():
+    print("Hello World Welcome")
+
+greet()
